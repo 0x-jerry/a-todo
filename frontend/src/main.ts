2545e35cbe5c960713da42c0c9b9d671912514f0
Jerry Wang <x.jerry.wang@gmail.com>
2025-01-28 11:01:39 +0800

    feat: basic layout

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,13 @@
-import { createApp, Plugin } from 'vue'
+import { createApp, type Plugin } from 'vue'
+import { createVuetify } from 'vuetify'
 import App from './App.vue'
 
 import './styles/global.less'
 import 'uno.css'
 
 const app = createApp(App)
+
+app.use(createVuetify())
 
 // install all modules
 Object.values(import.meta.glob<{ install: Plugin }>('./modules/*.ts', { eager: true })).forEach(
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,16 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
-import path from 'path'
+import path from 'node:path'
 import VueRouter from 'unplugin-vue-router/vite'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
-export default defineConfig(({}) => {
+export default defineConfig(() => {
   return {
     base: './',
     resolve: {
@@ -25,7 +26,16 @@ export default defineConfig(({}) => {
       }),
 
       // ⚠️ Vue must be placed after VueRouter()
-      Vue(),
+      Vue({
+        template: { transformAssetUrls },
+      }),
+
+      vuetify({
+        autoImport: true,
+        styles: {
+          configFile: 'src/styles/vuetify.scss',
+        },
+      }),
 
       // https://github.com/antfu/vite-plugin-components
       Components({
